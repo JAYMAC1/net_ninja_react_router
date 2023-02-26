a2e578f23aa5d4b76cacd56b10231cc99ab20f58
James McNeil <mcneil_james@hotmail.com>
2023-02-26 23:25:51 +0000

    changing to React Routetr Layouts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
 // Pages & Components
 import Home from './pages/Home'
 import About from './pages/About'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='about'>About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+// Layouts
+import RootLayout from './layouts/RootLayout'
+
+// create Browser Router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={RootLayout}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+    </Route>
   )
+)
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
