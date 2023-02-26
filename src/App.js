98d9aab4803b5575ff2745a5140d92209cca1483
James McNeil <mcneil_james@hotmail.com>
2023-02-26 23:27:00 +0000

    changed to component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import RootLayout from './layouts/RootLayout'
 // create Browser Router
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={RootLayout}>
+    <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
     </Route>
