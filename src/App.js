b490e3ed0eca277b9ccf50b61238e53c2f5c203b
James McNeil <mcneil_james@hotmail.com>
2023-02-27 20:10:08 +0000

    styled help elements

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 // Pages & Components
 import Home from './pages/Home'
 import About from './pages/About'
+import Faq from './pages/help/Faq'
+import Contact from './pages/help/Contact'
 
 // Layouts
 import RootLayout from './layouts/RootLayout'
@@ -20,8 +22,8 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='help' element={<HelpLayout />}>
-        <Route path='faq' />
-        <Route path='contact' />
+        <Route path='faq' element={<Faq />} />
+        <Route path='contact' element={<Contact />} />
       </Route>
     </Route>
   )
